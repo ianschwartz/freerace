cefcb42e4c61391bef65b5ebd35e64d762cef781
Ian Schwartz <ianschwartz@Ians-MacBook-Pro.local>
2017-12-04 14:33:35 -0500

    Created the FAQ page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
+  get 'faq', to: 'static_pages#faq'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
